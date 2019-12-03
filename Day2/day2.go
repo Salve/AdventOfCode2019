37227431d92b7886b6007cd0212d670974556997
Salve Spinnangr <salve.spinnangr@altibox.no>
2019-12-03 01:51:23 +0100

    Day2 part2 OK

--- Day2/day2.go
+++ Day2/day2.go
@@ -14,10 +14,29 @@ const MULT int = 2
 const HALT int = 99
 
 func main() {
-	program := readIntcode("input")
-	program[1] = 12
-	program[2] = 2
-	fmt.Printf("Program utput: %v", runIntcode(program)[0])
+	inputprogram := readIntcode("input")
+
+	fmt.Println("-- Part 1:")
+	part1program := make([]int, len(inputprogram))
+	copy(part1program, inputprogram)
+	part1program[1] = 12
+	part1program[2] = 2
+	fmt.Printf("Program utput: %v\n\n", runIntcode(part1program)[0])
+
+	fmt.Println("-- Part 2:")
+	var verb, noun int
+	for noun = 0; noun < 100; noun++ {
+		for verb = 0; verb < 100; verb++ {
+			part2program := make([]int, len(inputprogram))
+			copy(part2program, inputprogram)
+			part2program[1] = noun
+			part2program[2] = verb
+			result := runIntcode(part2program)[0]
+			if result == 19690720 {
+				fmt.Printf("Answer: %v", 100*noun+verb)
+			}
+		}
+	}
 }
 
 func runIntcode(program []int) []int {
@@ -26,15 +45,16 @@ func runIntcode(program []int) []int {
 		switch program[loc] {
 		case ADD:
 			program[program[loc+3]] = program[program[loc+1]] + program[program[loc+2]]
+			loc += 4
 		case MULT:
 			program[program[loc+3]] = program[program[loc+1]] * program[program[loc+2]]
+			loc += 4
 		case HALT:
+			loc += 1
 			return program
 		default:
 			log.Fatalf("Unexpected opcode: %v", program[loc])
 		}
-
-		loc += 4
 	}
 }
 
