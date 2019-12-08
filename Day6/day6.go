0e87f33fe24476e8efb57a49ac0f1c283900fcd7
Salve Spinnangr <salve.spinnangr@altibox.no>
2019-12-08 01:18:56 +0100

    Day6 part2 OK

--- Day6/day6.go
+++ Day6/day6.go
@@ -13,7 +13,36 @@ func main() {
 
 	fmt.Println("-- Part 1:")
 	count := countOrbits(orbiting)
-	fmt.Printf("Direct + indirect orbits: %d", count)
+	fmt.Printf("Direct + indirect orbits: %d\n\n", count)
+
+	fmt.Println("-- Part 2:")
+	fmt.Printf("Shortest distance: %d", shortestDist("YOU", "SAN", orbiting))
+}
+
+func shortestDist(a, b string, orbiting map[string]string) int {
+	apath := make(map[string]int)
+	system := a
+	dist := 0
+	for { // traverse from a to root, store all distances
+		if parent, exists := orbiting[system]; exists {
+			dist++
+			apath[parent] = dist
+			system = parent
+		} else {
+			break
+		}
+	}
+
+	dist = 0
+	system = b
+	for { // traverse from b towards root, until hitting path from a to root
+		parent := orbiting[system]
+		dist++
+		if adist, exists := apath[parent]; exists {
+			return dist + adist - 2 // don't count a and b
+		}
+		system = parent
+	}
 }
 
 func countOrbits(orbiting map[string]string) int {
--- Day6/day6_test.go
+++ Day6/day6_test.go
@@ -33,3 +33,47 @@ func Test_countOrbits(t *testing.T) {
 		})
 	}
 }
+
+func Test_shortestDist(t *testing.T) {
+	type args struct {
+		a        string
+		b        string
+		orbiting map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"example1",
+			args{
+				"YOU",
+				"SAN",
+				map[string]string{
+					"B":   "COM",
+					"C":   "B",
+					"D":   "C",
+					"E":   "D",
+					"F":   "E",
+					"G":   "B",
+					"H":   "G",
+					"I":   "D",
+					"J":   "E",
+					"K":   "J",
+					"L":   "K",
+					"YOU": "K",
+					"SAN": "I",
+				},
+			},
+			4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shortestDist(tt.args.a, tt.args.b, tt.args.orbiting); got != tt.want {
+				t.Errorf("shortestDist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
